de8eaf56b026dc59af967dad3fc4263b466d14e3
armmani <arm.maniii@gmail.com>
2025-06-04 17:16:47 +0700

    6

--- src/stores/postStore.js
+++ src/stores/postStore.js
@@ -1,3 +1,10 @@
+import axios from "axios";
 import { create } from "zustand";
 
-const usetPostStore = create( (set) => ({}) )
+const usetPostStore = create( (set) => ({
+  posts: [],
+  post: null,
+  actionFetchPostsByUserId: async (userId) => {
+    const res = await axios.get(` https://api-post-ts.onrender.com/api/v1/posts/${userId}`)
+  }
+}) )
