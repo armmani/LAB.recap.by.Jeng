8edf2b58690b286b4190e923ff47e43a00f13b5e
armmani <arm.maniii@gmail.com>
2025-06-04 17:26:48 +0700

    8

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import usetPostStore from '../stores/postStore'
 
 function PostPage() {
+
+  const actionFetchPostsByUserId = usetPostStore( state => state.actionFetchPostsByUserId )
+  const posts = usetPostStore( state => state.posts ) // ชื่อต้องตรงกันใน postStore
   return (
     <div>PostPage</div>
   )
